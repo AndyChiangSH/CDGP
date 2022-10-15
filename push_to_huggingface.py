d296b91f532debc54448aa215098f81a5dad6f75
Andy Chiang <andy10801@gmail.com>
2022-10-15 10:55:12 +0800

    Updata gitignore

--- push_to_huggingface.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForMaskedLM
-
-MODEL = "./models/CSG/BERT_CLOTH_model"
-PRETRAIN_MODEL = "bert-base-uncased"
-HUGGINGFACE_MODEL = ""
-
-print(f"Load CSG model at {MODEL}...")
-tokenizer = AutoTokenizer.from_pretrained(PRETRAIN_MODEL)
-csg_model = AutoModelForMaskedLM.from_pretrained(MODEL)
